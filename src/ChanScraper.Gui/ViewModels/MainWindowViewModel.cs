cf9d60628dc50bd592ea389cf795e8b858f92eb7
jyhe <jyhe.mark@hurtigruten.com>
2024-01-05 17:47:36 +1100

    Scrape component view style changes

--- src/ChanScraper.Gui/ViewModels/MainWindowViewModel.cs
+++ src/ChanScraper.Gui/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,21 @@
-﻿namespace ChanScraper.Gui.ViewModels;
+﻿using ReactiveUI;
+
+namespace ChanScraper.Gui.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    private string _downloadDirectory = string.Empty;
+
     public MainWindowViewModel()
     {
         ScrapeThread = new ScrapeThreadViewModel();
     }
-    
+
     public ScrapeThreadViewModel ScrapeThread { get; set; }
+
+    public string DownloadDirectory
+    {
+        get => _downloadDirectory;
+        set => this.RaiseAndSetIfChanged(ref _downloadDirectory, value);
+    }
 }
