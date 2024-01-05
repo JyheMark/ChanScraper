8fa212284dd1a49fa2f926757a792eac3e642ff9
jyhe <jyhe.mark@hurtigruten.com>
2024-01-05 18:02:23 +1100

    Added new scraper button

--- src/ChanScraper.Gui/Views/MainWindow.axaml.cs
+++ src/ChanScraper.Gui/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using ChanScraper.Gui.ViewModels;
 
 namespace ChanScraper.Gui.Views;
 
@@ -7,5 +9,20 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        AppendNewScrapeComponent();
+    }
+
+    private void Btn_AddScraper_OnClick(object? sender, RoutedEventArgs e)
+    {
+        AppendNewScrapeComponent();
+    }
+
+    private void AppendNewScrapeComponent()
+    {
+        var scrapeView = new ScrapeThreadView();
+        var scrapeViewModel = new ScrapeThreadViewModel();
+        scrapeView.DataContext = scrapeViewModel;
+        
+        pnl_ScrapeThreadAppend.Children.Add(scrapeView);
     }
 }
